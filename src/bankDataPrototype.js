bc4e8a87ec9f0b577f88937dd41aa28d2d03a139
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2018-05-30 10:00:35 +0200

    Fill IBAN only when user is finished typing

--- src/bankDataPrototype.js
+++ src/bankDataPrototype.js
@@ -1,40 +1,80 @@
 new Vue ( {
 
 	el: '#bankdata-prototype',
-	data: {
-			iban: ''
+	data: function () {
+		let iban = '',
+			validIBAN = 'DE00123456789012345678',
+			validBIC = 'DEUTDEBBXXX',
+			validKontonummer = validIBAN.substr(12, 10);
+			validBankleitzahl = validIBAN.substr(4, 8);
+		return {
+			iban: iban,
+			isValid: true,
+			validBankData: {
+				iban: validIBAN,
+				bic: validBIC,
+				konto: validKontonummer,
+				bankl: validBankleitzahl
+			}
+		}
 	},
 	computed: {
 		placeholder: function () {
 			let bic = document.getElementById( 'bic' );
 			if ( this.isIBAN() ) {
+				let validIBAN = this.isValidIBAN();
 				if ( this.isGermanIBAN() ) {
-					bic.value = '12345678';
-					bic.readOnly = true;
-					return '12345678'
+					bic.readOnly = validIBAN;
 				}
-				else
-					bic.readOnly = false;
-					return 'BIC';
+				this.isValid = validIBAN;
+				return 'BIC';
 			}
 			else if ( this.isKontonummer() ) {
-				bic.readOnly = false;
+				let validKontonummer = this.isValidKontonummmer();
+				this.isValid = validKontonummer;
+				bic.readOnly = validKontonummer;
 				return "Bankleitzahl"
 			}
 			bic.readOnly = false;
+			this.isValid = true;
 			bic.value = '';
 			return "BIC/Bankleitzahl"
+		},
+		classObject: function() {
+			return {
+				'field-grp': true,
+				'field-iba': true,
+				'invalid': !this.isValid,
+				'valid': this.isValid && !this.isFieldEmpty()
+			}
 		}
 	},
 	methods: {
+		isFieldEmpty: function () {
+			return this.iban === '';
+		},
 		isIBAN: function () {
 			return /^[A-Z]+([0-9]+)?$/.test( this.iban );
+		},
+		isValidIBAN: function () {
+			return ( this.iban === this.validBankData.iban ) || ( this.iban === '' );
 		},
 		isGermanIBAN: function () {
 			return /^(DE).*$/.test( this.iban );
 		},
 		isKontonummer: function () {
 			return /^\d+$/.test( this.iban );
+		},
+		isValidKontonummmer: function () {
+			return ( this.iban === this.validBankData.konto ) || ( this.iban === '' );
+		},
+		inputCompleted: function () {
+			return this.iban.length === 22;
+		},
+		fillBIC: function () {
+			if ( this.isValid ) {
+				document.getElementById( 'bic' ).value = this.isIBAN() ? this.validBankData.bic : this.validBankData.bankl;
+			}
 		}
 	}
 
